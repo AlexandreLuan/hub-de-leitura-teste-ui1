99763a6d56c461f21edc8bfd361ff03fc9dfbe30
Alexandre Luan <alexandreluand@gmail.com>
2026-09-08 21:40:48 -0300

    Automação do Contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,59 @@
+describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+
+  });
+
+  it.only('Deve preencher o formulário de contato com sucesso', () => {
+
+    cy.get('input[name="name"]').type('Alexandre Luan')
+    cy.get('input[name="email"]').type('alexandreluand@gmail.com')
+    cy.get('select[name="subject"]').select('Sugestões')
+    cy.get('textarea[name="message"]').type('Gostaria de sugerir a inclusão de um novo recurso no site.')
+    cy.get('button[type="submit"]').click()
+    //Resultado esperado
+    cy.contains('Contato enviado com sucesso!').should('exist')
+  });
+
+  it.only('Deve validar mensagem de erro ao enviar sem preencher nome', () => {
+    cy.get('input[name="name"]').clear()
+    cy.get('input[name="email"]').type('alexandreluand@gmail.com')
+    cy.get('select[name="subject"]').select('Sugestões')
+    cy.get('textarea[name="message"]').type('Gostaria de sugerir a inclusão de um novo recurso no site.')
+    cy.get('button[type="submit"]').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome.')
+
+  });
+
+  it.only('Deve validar mensagem de erro ao enviar sem preencher email', () => {
+    cy.get('input[name="name"]').type('Alexandre Luan')
+    cy.get('input[name="email"]').clear()
+    cy.get('select[name="subject"]').select('Sugestões')
+    cy.get('textarea[name="message"]').type('Gostaria de sugerir a inclusão de um novo recurso no site.')
+    cy.get('button[type="submit"]').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail.')
+
+  });
+
+  it.only('Deve validar mensagem de erro ao enviar sem selecionar o assunto', () => {
+    cy.get('input[name="name"]').type('Alexandre Luan')
+    cy.get('input[name="email"]').type('alexandreluand@gmail.com')
+    //cy.get('select[name="subject"]').select('Sugestões')
+    cy.get('textarea[name="message"]').type('Gostaria de sugerir a inclusão de um novo recurso no site.')
+    cy.get('button[type="submit"]').click()
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto.')
+
+  });
+
+  it.only('Deve validar mensagem de erro ao enviar sem preencher a mensagem', () => {
+    cy.get('input[name="name"]').type('Alexandre Luan')
+    cy.get('input[name="email"]').type('alexandreluand@gmail.com')
+    cy.get('select[name="subject"]').select('Sugestões')
+    //cy.get('textarea[name="message"]').type('Gostaria de sugerir a inclusão de um novo recurso no site.')
+    cy.get('button[type="submit"]').click()
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem.')
+
+  });
+
+});
