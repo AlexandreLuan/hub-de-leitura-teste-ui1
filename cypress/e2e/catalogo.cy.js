177e96354fe5323adb085af870dfe09148c43c40
Alexandre Luan <alexandreluand@gmail.com>
2026-09-10 10:41:02 -0300

    automação da lista de catalogo

---
+++ cypress/e2e/catalogo.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade: Catálogo de Livros', () => {
+
+    beforeEach(() => {
+
+        cy.visit('http://localhost:3000/catalog.html')
+    });
+
+    it.skip('Deve clicar no botão Adicionar à cesta', () => {
+        cy.get(':nth-child(1) > .card > .card-body > .mt-auto > .d-grid > .btn-primary').click()
+        cy.get('#cart-count').should('contain', 1)
+    });
+
+    it('Deve clicar em todos os botões Adicionar à cesta', () => {
+        cy.get('.btn-primary').click({ multiple: true })
+    });
+
+    it('Deve clicar no primeiro botão Adicionar à cesta', () => {
+        cy.get('.btn-primary').first().click()
+
+    });
+
+    it('Deve clicar no último botão Adicionar à cesta', () => {
+        cy.get('.btn-primary').last().click()
+    });
+
+    it('Deve clicar no terceiro botão Adicionar à cesta', () => {
+        cy.get('.btn-primary').eq(2).click()
+        cy.get('#global-alert-container').should('contain', 'A Divina Comédia')
+    });
+
+    it('Deve clicar no nome do livro e direcionar para a tela do livro', () => {
+        cy.contains('A Divina Comédia').click()
+        cy.url().should('include', 'book-details.html')
+        cy.get('#add-to-cart-btn').click()
+        cy.get('#alert-container').should('contain', 'Livro adicionado à cesta com sucesso!')
+        
+    });
+});
